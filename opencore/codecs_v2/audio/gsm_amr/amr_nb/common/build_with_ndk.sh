35a6cc51347ab7aa13f14ea716523a4b25918b5a
panzy/work-fed <panzhiyong84@gmail.com>
2014-10-28 17:34:53 +0800

    relative path in build script.

--- opencore/codecs_v2/audio/gsm_amr/amr_nb/common/build_with_ndk.sh
+++ opencore/codecs_v2/audio/gsm_amr/amr_nb/common/build_with_ndk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # panzy, 2014-10-28
-export PV_TOP=/home/panzy/workspace/opencore-amr-0.1.2/opencore/codecs_v2/audio/gsm_amr/amr_nb/../../../../
+export PV_TOP=`pwd`/../../../../../
 
 ls -l $PV_TOP/codecs_v2/audio/gsm_amr/common/dec/include 
 ndk-build NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=Android.mk NDK_APPLICATION_MK=Application.mk $@
